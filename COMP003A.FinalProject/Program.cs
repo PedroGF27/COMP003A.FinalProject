cbe2e1063c9135a503c76dc36722baf24afb0007
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-03-02 16:34:06 -0800

    Add project files.

---
+++ COMP003A.FinalProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// Pedro Garcia
+// COMP003A
+// Johnathan Cruz
+// Inventory Tracking System
+namespace COMP003A.FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
