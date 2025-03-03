9138d679ef54d4c4ade8b10bde68b7d6e22d6259
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-03-02 18:06:43 -0800

    Added InventoryUtility class. Added switch case to main that might change.

---
+++ COMP003A.FinalProject/InventoryUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.FinalProject
+{
+    public class InventoryUtility
+    {
+        /// <summary>
+        /// Capture user Input of item name, amount, and cost.
+        /// </summary>
+        /// <param name="inventoryName"></param>
+        /// <param name="inventoryAmount"></param>
+        /// <param name="cost"></param>
+        public void InventoryDescription (string inventoryName)
+        {
+            Console.WriteLine($"Item Name: {inventoryName}");
+        }
+
+        public void InventoryDescription(string inventoryName, int inventoryAmount)
+        {
+            Console.WriteLine($"Item Name: {inventoryName}, Inventory Amount: {inventoryAmount}");
+        }
+
+        public void InventoryDescription(string inventoryName, int inventoryAmount, double cost)
+        {
+            Console.WriteLine($"Item Name: {inventoryName}, Inventory Amount: {inventoryAmount}, Inventory Cost: {cost}");
+        }
+    }
+}
--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -8,12 +8,70 @@ namespace COMP003A.FinalProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("- - - Inventory Tracker - - -");
-            Console.WriteLine("1. Add an Item");
-            Console.WriteLine("2. Remove an Item");
-            Console.WriteLine("3. Edit an Item");
-            Console.WriteLine("4. View full Inventory");
-            Console.WriteLine("5. Exit");
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("- - - Inventory Tracker - - -");
+                Console.WriteLine("1. Add an Item");
+                Console.WriteLine("2. Remove an Item");
+                Console.WriteLine("3. Edit an Item");
+                Console.WriteLine("4. View full Inventory");
+                Console.WriteLine("5. Exit");
+                Console.Write("(1-5): ");
+                int choice;
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input. Choose a number 1-5");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            InventoryUtility ItemDescription = new InventoryUtility();
+                            Console.WriteLine("Item/Inventory name: ");
+                            string inventoryName = Console.ReadLine();
+
+                            Console.WriteLine("Item/Inventory Amount: ");
+                            int inventoryAmount = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Cost of item/inventory: ");
+                            double cost = Convert.ToDouble(Console.ReadLine());
+                            
+                            break;
+                        }
+                    case 2:
+                        {
+
+                            break;
+                        }
+                    case 3:
+                        {
+
+                            break;
+                        }
+                    case 4: 
+                        {
+
+                            break;
+                        }
+                    case 5: 
+                        {
+                            exit = true;
+                            Console.WriteLine("Ending Program....");
+                            break;
+                        }
+
+                }
+
+
+            }
         }
     }
 }
